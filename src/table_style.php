f2dcd63afc47495458d0d89db53dc672a1de6471
sandikodev <androxoss@hotmail.com>
2024-01-17 05:56:12 +0700

    update table

---
+++ src/table_style.php
@@ -0,0 +1,21 @@
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th,
+    td {
+        text-align: left;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    th {
+        color: white;
+        background: black;
+    }
+</style>
--- src/view_agenda.php
+++ src/view_agenda.php
@@ -10,7 +10,6 @@
             <th>pukul</th>
             <th>tempat</th>
             <th>agenda</th>
-            <th>tindakan</th>
         </thead>
         <tbody>
             <?php
@@ -25,12 +24,6 @@
                 echo "<td>" . $activities['pukul'] . "</td>";
                 echo "<td>" . $activities['tempat'] . "</td>";
                 echo "<td>" . $activities['acara'] . "</td>";
-
-                // column action
-                echo "<td>";
-                echo "<a href='form_edit.php?id=" . $activities['id'] . "'>sunting</a> | ";
-                echo "<a href='action/del_activity.php?id=" . $activities['id'] . "'>hapus</a>";
-                echo "</td>";
                 echo "</tr>";
                 $index++;
             } ?>
